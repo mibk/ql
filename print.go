621311f33e179ad61219adc0e4df85c2d3ac90a1
Michal Bohuslávek <mbohuslavek@gmail.com>
2015-07-23 12:14:03 +0200

    implement String for builders

---
+++ print.go
@@ -0,0 +1,29 @@
+package ql
+
+type sqlBuilder interface {
+	ToSql() (string, []interface{})
+}
+
+func makeSql(b sqlBuilder) string {
+	sql, err := Interpolate(b.ToSql())
+	if err != nil {
+		panic(err)
+	}
+	return sql
+}
+
+func (b *DeleteBuilder) String() string {
+	return makeSql(b)
+}
+
+func (b *InsertBuilder) String() string {
+	return makeSql(b)
+}
+
+func (b *SelectBuilder) String() string {
+	return makeSql(b)
+}
+
+func (b *UpdateBuilder) String() string {
+	return makeSql(b)
+}
