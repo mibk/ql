abe858ae6ae68918658cabd3eb88e31aa698bfa8
Michal Bohuslávek <mbohuslavek@gmail.com>
2015-07-28 12:46:17 +0200

    comment out the String methods

--- print.go
+++ print.go
@@ -12,22 +12,27 @@ func makeSql(b queryBuilder) string {
 	return sql
 }
 
+// String returns a string representing a preprocessed, interpolated, query.
 func (q *Query) String() string {
 	return makeSql(q)
 }
 
+// String returns a string representing a preprocessed, interpolated, query.
 func (b *DeleteBuilder) String() string {
 	return makeSql(b)
 }
 
+// String returns a string representing a preprocessed, interpolated, query.
 func (b *InsertBuilder) String() string {
 	return makeSql(b)
 }
 
+// String returns a string representing a preprocessed, interpolated, query.
 func (b *SelectBuilder) String() string {
 	return makeSql(b)
 }
 
+// String returns a string representing a preprocessed, interpolated, query.
 func (b *UpdateBuilder) String() string {
 	return makeSql(b)
 }
